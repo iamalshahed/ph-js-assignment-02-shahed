dfa4016f652dc0e02164691b8187941abe512229
iamalshahed <shahedevise@gmail.com>
2025-08-14 18:06:05 +0600

    problem-1 done

---
+++ problem_1/problem_1.js
@@ -0,0 +1,11 @@
+function totalFine(fare) {
+  // check input is number or not
+  if (typeof fare !== "number" || fare !== Math.abs(fare) || fare === 0) {
+    return "Invalid";
+  }
+
+  const fine = fare + (fare * 20) / 100 + 30;
+  return fine;
+}
+const fine = totalFine(0);
+console.log(fine);
